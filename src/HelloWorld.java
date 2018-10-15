440dec4bd0c38ba98d45d9f9affe51136d23deaf
seven academy <sevenacademy@sevens-iMac.local>
2018-10-15 13:24:46 +0100

    Added my name to the file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,10 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello guys! My name is Akah");
-		System.out.println("Learning Java at Seven Academy");
-		System.out.println("Setting up the git repository");
-		
+		System.out.println("Hello guys! My name is Thierry Leutou");
 
 	}
 
