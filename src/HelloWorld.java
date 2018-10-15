d00ec657b938a603e89edabbd3eda5b39f4b12c6
seven academy <sevenacademy@sevens-iMac.local>
2018-10-15 13:25:23 +0100

    update to add only my name TCHOUMI

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello guys! My name is Akah");
-		System.out.println("Learning Java at Seven Academy");
-		System.out.println("Setting up the git repository");
+		System.out.println("Hello guys! My name is mister TCHOUMI");
+		
 		
 
 	}
