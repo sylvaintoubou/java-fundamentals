b12be80e5df47bbd875e107d9b3f0dbe65ee259f
seven academy <sevenacademy@sevens-iMac.local>
2018-10-15 12:47:55 +0100

    Added 3rd line in my method main

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello guys! My name is Akah");
 		System.out.println("Learning Java at Seven Academy");
+		System.out.println("Setting up the git repository");
+		
 
 	}
 
