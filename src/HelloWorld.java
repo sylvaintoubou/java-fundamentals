27c8d4f06e2df4b2ddd0c4349690f2d10163f8cb
= <sevenacademy@sevens-iMac-2.local>
2018-10-15 12:02:39 +0100

    New update

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello guys! My name is Akah");
+		System.out.println("Learning Java at Seven Academy");
+
+	}
+
+}
