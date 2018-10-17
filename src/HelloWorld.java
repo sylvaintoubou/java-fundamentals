52b4c30524502bbe42cdcfd5bf43b9ee26a9b2e2
larrytech7 <larryakah@gmail.com>
2018-10-17 12:59:55 +0100

    Practical session number 3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,16 @@
 
 public class HelloWorld {
 
+	static short age = 127; 
+	
 	public static void main(String[] args) {
 		System.out.println("Akah");
 		System.out.println("Hello guys! My name is mister TCHOUMI");
 		System.out.println("Hello guys! My name is Leutou");
 		System.out.println("Hello guys! My name is TCHETCHOUA");
 		System.out.println("Ndeme yvan rodrigue marie");
+		
+		System.out.println(age);
 
 	}
 
