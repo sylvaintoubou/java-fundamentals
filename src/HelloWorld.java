4ec435bfa945ec5bb8a44523e914ea6fcd37ba66
seven academy <sevenacademy@sevens-iMac.local>
2018-10-17 13:15:38 +0100

    variables and datatypes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,50 @@
 
 public class HelloWorld {
-
+          static long age = 9223372036854775807l;
+          static float money = 4500.25f; 
+          static double mesure = 5.562332;
+          static boolean Love = true;
+          static char study = 't';
+          static Double house;
+          static HelloWorld president;
+          public static int k = 502;
+          static float a,b,c,d=a=b=c=7.5f;
 	public static void main(String[] args) {
 		System.out.println("Akah");
 		System.out.println("Hello guys! My name is mister TCHOUMI");
 		System.out.println("Hello guys! My name is Leutou");
 		System.out.println("Hello guys! My name is TCHETCHOUA");
 		System.out.println("Ndeme yvan rodrigue mari");
-
+        System.out.println(age);
+        System.out.println(money);
+        System.out.println(mesure);
+        System.out.println(Love);
+        System.out.println(study);
+        System.out.print(house);
+        System.out.println("\n");
+        System.out.println(president);
+        System.out.println("i am a boy\r could you see me");
+        System.out.println("will you give it to me\f could you hear me");
+        System.out.println("love often you do\t with me ");
+        System.out.println("love often you \"do\" with me ");
+        System.out.println("jupiter is the most popular and mars is the less popular\u9650");
+        System.out.println("jupiter is the most popular and mars is the less popular");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        int t = 29;
+        System.out.println(t);
+        System.out.println(k);
+        Bonjour right = new Bonjour();
+        right.changely();
 	}
-
+    	
+    
+}
+class Bonjour{
+	public  void changely() {
+		System.out.println(HelloWorld.k);
+	}
+		
 }
