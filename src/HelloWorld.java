d15db62261a63dbfa6b069efb2acf61c3825cb77
ndemeyvan <sevenacademy@sevens-iMac.local>
2018-10-17 13:04:16 +0100

    modification fondamental

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,50 @@
 
 public class HelloWorld {
-
+	
+	static long age = 9223372036854775807l;
+	static float virgule1 = 1.23f ;
+	static double virgule2 = 5.5;
+	static boolean oklm= true ;
+	static char cher= 'z';
+	static Double score ;
+	static HelloWorld good;
+	static float float1=2,float2=2,float3=2,float4=2;
+	static int a,b,c=a=b=1;
+	public static int bonbon =35;
+	
 	public static void main(String[] args) {
+		
+		int age = 12;
 		System.out.println("Akah");
 		System.out.println("Hello guys! My name is mister TCHOUMI");
 		System.out.println("Hello guys! My name is Leutou");
 		System.out.println("Hello guys! My name is TCHETCHOUA");
 		System.out.println("Ndeme yvan rodrigue mari");
-
+		System.out.println(age);
+		System.out.println(virgule1);
+		System.out.println(virgule2);
+		System.out.println(oklm);
+		System.out.println(cher);
+		System.out.println(score);
+		System.out.println("i am a seven academy guys");
+		System.out.println(float1);
+		System.out.println(float2);
+		System.out.println(float3);
+		System.out.println(float4);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(bonbon);
+		
 	}
+	
+}
 
+ class Bonjour {
+	
+	public void name(String[] args) {
+		
+		System.out.println(HelloWorld.bonbon);
+	}
+	
 }
