cc799751e8ea365a40ca2239f3c05f68c9350b0f
larrytech7 <larryakah@gmail.com>
2019-01-02 02:07:09 -0800

    Added the recent changes to git

--- classlabs/src/classlabs/MainClass.java
+++ classlabs/src/classlabs/MainClass.java
@@ -1,22 +1,39 @@
 package classlabs;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		User myUser = new User();
 		Scanner inputScanner = new Scanner(System.in);
+		System.out.println("GAPFUL NUMBER DETECTION");
+		String test = "";
 		do {
-			System.out.println("Hello Please enter your email : ");
-			String email = inputScanner.nextLine();
-			myUser.setEmail(email);
-			System.out.println("\nEnter your password : " );
-			String password = inputScanner.nextLine();
-			myUser.setPassword(password);
-			System.out.println(myUser.errorMessage);
-		}while(myUser.password == null || myUser.email == null);
+			System.out.print("Hello Please enter A NUMBER : ");
+			test = inputScanner.nextLine();
+			boolean result = isGapful(test);
+			System.out.println(test+ (result ? " is a gapful number" : " is not  gapful number"));
+		}while(test.length() >= 3);
 		
+	}
+
+	private static boolean isGapful(String test) {
+		Character firstNumber = test.charAt(0);
+		Character lastNumber = test.charAt(test.length()-1);
+		int fullNumber = Integer.parseInt(test);
+		return fullNumber % Integer.parseInt(String.valueOf(firstNumber.toString()+lastNumber.toString())) == 0;
+	}
+	
+	private static int[][] rotateMatrix(int[][] x, int deg) {
+		
+		int rotatingFactor = deg/90; //rotating factor as as multiple of 90
+		int[][] tmp = new int[x.length][x.length];
+		for(int i=0; i < x.length; i++) {
+			for(int k=0; k<x[i].length; k++)
+				tmp[i][x[i].length - rotatingFactor] = x[i][k];
+		}
+		return tmp;
 	}
 
 }
