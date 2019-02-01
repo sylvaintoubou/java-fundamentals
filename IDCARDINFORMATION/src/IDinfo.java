7219eb1c651f436558fae443ad64fbd6afee2413
Udumandy <udumandy@yahoo.com>
2019-02-01 05:08:29 +0100

    Added id card information

---
+++ IDCARDINFORMATION/src/IDinfo.java
@@ -0,0 +1,25 @@
+
+public class IDinfo {
+
+	public static void main(String[] args) {
+		String Number = "NW01396151PGOEAPRSJ1" , name = "Uduma" , surnames = "Andreas Mokonchu" ,
+				      Fathername = "PND" , Mothername = "Ayota Mokonchu Marceline" ,
+				      place = "Bamenda" , Height = "165cm" , Occupation = "Applicant" , 
+					  DOB = "15.09.1983" , DOI = "06.07.2018" , DOE = "06.07.2023" ;
+		System.out.println("ID Card number : "+Number);
+		System.out.println("Name : "+name);
+		System.out.println("Surnames : "+surnames);
+		System.out.println("Father's Name : "+Fathername);
+		System.out.println("Mother's Name : "+Mothername);
+		System.out.println("Born on : "+DOB+ " At : "+place);
+		System.out.println("Height : "+Height);
+		System.out.println("Occupation : "+Occupation);
+		System.out.println("Date of Issue : "+DOI+ " Valid till : "+DOE);
+		
+		
+		
+		
+
+	}
+
+}
