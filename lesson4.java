de2dec2025ad9726bc32374a61dec980caa28963
Udumandy <udumandy@yahoo.com>
2019-02-04 20:37:28 +0100

    assignments on lesson 4

---
+++ lesson4.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class lesson4 {
+
+	public static void main(String[] params) {
+		//String title = params[0];
+		int num1 =10; float num2 = 5.5f;
+		double output = (num1+num2 ) * 0.5 - 0.5 / 2;  //7.5
+		//System.out.println("Title : "+ title );
+		System.out.println("Output is "+ output);
+		System.out.println("num1 shifted one place to the Left: "+ (num1<<2));//4
+
+	}
+
+}
---
+++ lesson4cont.java
@@ -0,0 +1,19 @@
+package homework;
+
+public class lesson4cont {
+
+	public static void main(String[] params){//static 
+		int sum = 1 ;
+		int _a = 30; 
+		int _b = 10;
+		sum = _a + _b;
+		System.out.println("Sum: "+sum);
+		System.out.println("Sum again: "+sum++);
+		System.out.println("Sum again : "+ (--sum)%2);
+		sum /= 2;
+		//System.out.println("True or false? "+(sum + _b)==_a?True:False);
+		}
+
+	}
+
+
